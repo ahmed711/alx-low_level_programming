590b543457f9b9b2fec3c4421005d389a3a56f64
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-10 05:43:05 -0700

    adding ex 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Print a sentance to screen'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
