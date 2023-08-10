482ad0bbe1c78b731d4e0c8d4971f48af9fe1342
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-10 07:21:48 -0700

    try

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'Print a sentance to screen'
+ * Return: Always 1 (Failure)
+ */
+
+int main(void)
+{
+system("echo 'and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19' > /dev/stderr\n");
+return (1);
+}
