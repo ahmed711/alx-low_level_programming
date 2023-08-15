2038b0c360f3091f397973386e30925c3d423029
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-15 14:00:37 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Print a sentance to screen'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int sum, i;
+
+	sum = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
