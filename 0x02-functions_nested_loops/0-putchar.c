8da5f67dee3f9815453014e4937aae128bdcc561
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-15 02:22:50 -0700

    added main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,13 @@
 
 int main(void)
 {
-    int i, j;
+	int i;
+	char word[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 
-    char word[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-    j = sizeof(word);
-    for(i = 0; i < j; i++)
-     {
-         _putchar(word[i]);
-     }
-     _putchar('\n');
-     
-     return 0;
+	for (i = 0; i < (int) sizeof(word); i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
