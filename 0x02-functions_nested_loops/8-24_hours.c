b84ca60983b957dcbc89c61e089083ab83750d79
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-15 10:23:14 -0700

    Print minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * Description: 'Print a sentance to screen'
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i > 1 && j > 3)
+			{
+				break;
+			}
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
