4344835180394c2dd110833f12e078d74aa88c89
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-17 03:38:03 -0700

    Multiply 2 nums

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: first int
+ * @b: second int
+ * Description: 'Check if a character is uppercase'
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
