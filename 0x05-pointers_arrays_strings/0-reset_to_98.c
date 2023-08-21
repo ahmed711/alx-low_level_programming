86347870302814b142313e201c3717d650e5d36c
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-21 07:40:16 -0700

    Ex 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n: pointer to n address
+ * Description: 'Check if a character is uppercase'
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
