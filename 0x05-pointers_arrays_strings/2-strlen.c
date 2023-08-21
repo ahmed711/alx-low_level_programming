68460295e2d5426bff1e24064b509315972f6e4b
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-21 12:13:54 -0700

    Strlen func

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: pointer to n address
+ * Description: 'Check if a character is uppercase'
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
