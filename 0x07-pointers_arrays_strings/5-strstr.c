31692c3ca50f0bf93cc2f04833ff34e222846111
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-28 12:28:47 -0700

    last one

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: string s
+ * @needle: string accept
+ * Description: 'find c in s'
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, k, l, count;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+	}
+
+	if (j > i)
+	{
+		return ((void *)0);
+	}
+
+	count = 0;
+	for (k = 0; k < i - j + 1; k++)
+	{
+		for (l = 0; needle[l] != '\0'; l++)
+		{
+			if (haystack[k + l] == needle[l])
+			{
+				count++;
+				if (count == j)
+				{
+					return (haystack + k);
+				}
+			}
+			else
+			{
+				count = 0;
+				break;
+			}
+		}
+	}
+
+	return ((void *)0);
+}
