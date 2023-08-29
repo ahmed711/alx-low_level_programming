2728e22a0557d1d215d24e3173adda72a0bd3ac1
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-28 21:42:03 -0700

    Bonus 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: string dest
+ * @to: string b
+ * Description: 'fill the first n bytes of s with b'
+ * Return: Always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
