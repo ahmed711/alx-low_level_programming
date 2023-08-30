4753c1be46b5dfa4e7db6d6fc18ae211d18b92c6
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-08-30 06:13:37 -0700

    08 first ex

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * @s: string
+ * Description: 'fill the first n bytes of s with b'
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
