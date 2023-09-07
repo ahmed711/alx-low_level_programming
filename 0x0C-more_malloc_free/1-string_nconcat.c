d5480cc6e89dd03d7c66c284aa89eba668dfec9d
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-09-07 17:24:43 +0200

    Update size

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, j, k;
+	unsigned int i, j, size;
 
 	for (i = 0; s1 && *(s1 + i) != '\0'; i++)
 	{
@@ -23,16 +23,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 	}
 
-	k = n > j ? (i + j) : (i + n);
+	size = n > j ? (i + j) : (i + n);
 
-	s = malloc((k + 1) * sizeof(char));
+	s = malloc((size + 1) * sizeof(char));
 	if (s == NULL)
 	{
 		free(s);
 		return (NULL);
 	}
 
-	for (j = 0; j < k; j++)
+	for (j = 0; j < size; j++)
 	{
 		if (j < i && s1)
 		{
@@ -43,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			s[j] = s2[j - i];
 		}
 	}
-	s[k] = '\0';
+	s[size] = '\0';
 
 	return (s);
 }
