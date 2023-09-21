493be49b80ba44fdbd48f4018fb68d60ad741092
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-09-21 16:41:09 -0700

    first bonus

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_before_main - function with constructor
+ *
+ * Description: print string
+ * Return: nothing
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
