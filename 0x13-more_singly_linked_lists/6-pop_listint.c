69cf270ee1abb098beb98bd9df19e48158cdab86
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-09-25 19:12:44 -0700

    Ex 7

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Entry point
+ * @head: nodes
+ * Description: 'Check if a character is uppercase'
+ * Return: Always 0 (Success)
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int i;
+
+	tmp = head && *head ? *head : NULL;
+	if (tmp)
+	{
+		i = tmp->n;
+		tmp = tmp->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	return (i);
+}
