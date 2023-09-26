0a55454c37b80c4856a82f6d19128c30109f9c92
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-09-25 18:52:13 -0700

    Ex 6

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Entry point
+ * @head: nodes
+ * Description: 'Check if a character is uppercase'
+ * Return: Always 0 (Success)
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	tmp = *head;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		free(*head);
+		*head = tmp;
+	}
+}
