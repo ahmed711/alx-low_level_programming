f04188ad3753369e33b77f0c585301a2f92f45f1
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-09-25 20:09:05 -0700

    Ex 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,6 +32,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp->next = node;
 		return (node);
 	}
+	else if (!tmp && !idx)
+	{
+		*head = node;
+		node->next = NULL;
+		return (node);
+	}
 
 	return (NULL);
 }
