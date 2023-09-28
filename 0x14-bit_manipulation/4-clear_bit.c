0f630cb2568635930c759da9740c5ae354870f28
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-09-28 16:46:58 -0700

    Ex 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * clear_bit - Entry point
+ * @n: unsigned int
+ * @index: unsigned int
+ * Description: 'Check if a character is uppercase'
+ * Return: Always 0 (Success)
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	mask = ~(1 << index);
+	*n = mask & *n;
+
+	return (1);
+}
