94a60612f7c16ea3d6f89254516fb511975ca3d1
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-10-02 18:50:47 -0700

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+
+#define BUFSIZE 1024
 
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
