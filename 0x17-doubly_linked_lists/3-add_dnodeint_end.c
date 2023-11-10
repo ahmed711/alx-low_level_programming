54b0b42dbf4ffa8750b51d4495ce544e1310f925
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-11-09 19:00:22 -0800

    h

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,7 +7,7 @@
  * Return: Always EXIT_SUCCESS.
  */
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *tmp;
 
