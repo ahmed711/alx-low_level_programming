559538e82778d1bbdab047be60fc1950d9e990ca
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-11-09 19:00:01 -0800

    h

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,7 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 	tmp = *head;
 	while (tmp->next)
-		tmp = tmp->next
+		tmp = tmp->next;
 	new_node->prev = tmp;
 	tmp->next = new_node;
 
